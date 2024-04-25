e7bdf71ae85545eff8e79776770caeee14515a21
omar <omarhrn@gmail.com>
2024-04-25 10:59:16 -0400

    Arreglado el problema de metadata

--- src/components/ui/promo-section/SocialMedia.tsx
+++ src/components/ui/promo-section/SocialMedia.tsx
@@ -1,30 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 
 const people = [
   {
     name: 'Facebook',
-    role: 'www.facebook.com/muscariclothing',
+    url: 'https://facebook.com',
     imageUrl:
       '/social-media/facebook.png',
   },
   {
     name: 'Instagram',
-    role: 'www.instagram.com/muscariclothing',
+    url: 'https://instagram.com',
     imageUrl:
       '/social-media/instagram.png',
   },
   
   {
     name: 'Tiktok',
-    role: 'www.tiktok.com/muscariclothing',
+    url: 'https://tiktok.com',
     imageUrl:
       '/social-media/tiktok.png',
   },
   {
     name: 'Twitter',
-    role: 'www.twitter.com/muscariclothing',
+    url: 'https://twitter.com',
     imageUrl:
       '/social-media/twitter.png',
   },
@@ -43,13 +44,12 @@ export default function SocialMedia() {
         <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
           {people.map((person) => (
             <li key={person.name}>
-              <div className="flex items-center gap-x-6">
+              <Link href={person.url} className="flex items-center gap-x-6">
                 <Image className="h-16 w-16 rounded-full" width={40} height={40} src={person.imageUrl} alt="" />
                 <div>
                   <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                  <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
                 </div>
-              </div>
+              </Link>
             </li>
           ))}
         </ul>
