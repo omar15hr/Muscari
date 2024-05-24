2084cdf6cbf5b3423d07dbce53a4ac7b3c50537b
omar <omarhrn@gmail.com>
2024-05-24 11:01:27 -0400

    Intento de pruebas

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -1,8 +1,6 @@
 import NextAuth, { DefaultSession } from 'next-auth';
 
 
-
-
 declare module 'next-auth' {
   interface Session {
     user: {
--- src/components/back-button/BackButton.tsx
+++ src/components/back-button/BackButton.tsx
@@ -3,7 +3,6 @@ import { IoArrowBack } from "react-icons/io5";
 
 export const BackButton = ({path}: any) => {
 
-
   return (
     <>
       <Link href={path} className='flex text-white w-40 hover:bg-blue-300 hover:text-black bg-blue-700 rounded px-4 mx-2 py-1 mb-2'>
