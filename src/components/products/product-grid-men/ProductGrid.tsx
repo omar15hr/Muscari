537f41b313f6ff004126bd310ffaca74cd78f968
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-05-12 09:13:26 -0400

    cambios en el select

--- src/components/products/product-grid-men/ProductGrid.tsx
+++ src/components/products/product-grid-men/ProductGrid.tsx
@@ -1,37 +1,37 @@
-'use client';
+"use client";
 
-import { Product } from '@/interfaces';
-import { ProductGridItem } from './ProductGridItem';
-import { useState } from 'react';
+import { Product } from "@/interfaces";
+import { ProductGridItem } from "./ProductGridItem";
+import { useState } from "react";
 
 import styles from "@/components/filters/styles.module.scss";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface Props {
   products: Product[];
 }
 
-
 export const ProductGrid = ({ products }: Props) => {
-
-
   const [filters, setFilters] = useState({
     title: `all`,
-    minPrice: 0
+    minPrice: 0,
   });
 
   const [minPrice, setMinPrice] = useState(0);
 
-
   const filterProducts = (products: Product[]) => {
-    return products.filter(product => {
+    return products.filter((product) => {
       return (
         product.price >= filters.minPrice &&
-        (
-          filters.title === 'all' ||
-          product.title.includes(filters.title)
-        )
-      )
-    })
+        (filters.title === "all" || product.title.includes(filters.title))
+      );
+    });
   };
 
   const filteredProducts = filterProducts(products);
@@ -41,59 +41,55 @@ export const ProductGrid = ({ products }: Props) => {
 
     setFilters((prevState: any) => ({
       ...prevState,
-      minPrice: event.target.value
-    }))
+      minPrice: event.target.value,
+    }));
   };
 
   const handleChangeTitle = (event: any) => {
     setFilters((prevState: any) => ({
       ...prevState,
-      title: event.target.value
+      title: event.target.value,
     }));
-  }
+  };
 
   return (
-    <div className='grid grid-col'>
-      <section className={styles.filters} >
-
-        <div className='mb-10 mx-5'>
+    <div className="grid grid-col">
+      <section className={styles.filters}>
+        <div className="mb-10 mx-5">
           <label htmlFor="filters-price">Precio a partir de:</label>
           <input
-            type='range'
-            id='filters-price'
+            type="range"
+            id="filters-price"
             defaultValue={0}
-            min='0'
-            max='300'
+            min="0"
+            max="300"
             className="bg-gray-300"
             onChange={handleChangeMinPrice}
           />
           <span>${filters.minPrice}</span>
         </div>
 
-        <div className='mb-10 mr-5'>
-          <label htmlFor="filters-title">Filtrar por título:</label>
-          <select id="filters-title" className='btn btn-secondary dropdown-toggle' onChange={handleChangeTitle} >
-            <option value="all">Todos</option>
-            <option value="Tee">Polera</option>
-            <option value="Hoodie">Poleron</option>
-            <option value="Sweatshirt">Sweater</option>
-            <option value="Jacket">Chaqueta</option>
-            <option value="Hat">Jockey</option>
-          </select>
-
+        <div className="mb-10 mr-5">
+          <Select onValueChange={handleChangeTitle}>
+            <SelectTrigger className="w-[200px]">
+              <SelectValue placeholder="Todos los productos" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">Todos</SelectItem>
+              <SelectItem value="Tee">Polera</SelectItem>
+              <SelectItem value="Hoodie">Poleron</SelectItem>
+              <SelectItem value="Sweatshirt">Sweater</SelectItem>
+              <SelectItem value="Jacket">Chaqueta</SelectItem>
+              <SelectItem value="Hat">Jockey</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
       </section>
 
-
       <div className="grid grid-cols-3 sm:grid-cols-5 gap-10 mb-10">
-        {
-          filteredProducts.map(product => (
-            <ProductGridItem
-              key={product.slug}
-              product={product}
-            />
-          ))
-        }
+        {filteredProducts.map((product) => (
+          <ProductGridItem key={product.slug} product={product} />
+        ))}
       </div>
     </div>
   );
